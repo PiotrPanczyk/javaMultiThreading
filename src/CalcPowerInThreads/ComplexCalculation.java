b9220ea5e03dce2c6d8ced4812102cbfde51b74d
Piotr Panczyk <piotr@tower.home>
2019-06-23 15:30:44 +0100

    Adding java files and gitignore

--- src/CalcPowerInThreads/ComplexCalculation.java
+++ src/CalcPowerInThreads/ComplexCalculation.java
@@ -1,4 +1,56 @@
 package CalcPowerInThreads;
 
+import java.math.BigInteger;
+
 public class ComplexCalculation {
+    public BigInteger calculateResult(BigInteger base1, BigInteger power1, BigInteger base2, BigInteger power2) {
+        BigInteger result;
+        PowerCalculatingThread t1 = new PowerCalculatingThread(base1, power1);
+        PowerCalculatingThread t2 = new PowerCalculatingThread(base2, power2);
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join(2000);
+            t2.join(2000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        if(t1.isAlive()) t1.interrupt();
+        if(t2.isAlive()) t2.interrupt();
+        result = t1.getResult().add(t2.getResult());
+        return result;
+    }
+
+    private static class PowerCalculatingThread extends Thread {
+        private BigInteger result = BigInteger.ONE;
+        private BigInteger base;
+        private BigInteger power;
+
+        public PowerCalculatingThread(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+           /*
+           Implement the calculation of result = base ^ power
+           */
+            for(BigInteger i = BigInteger.ZERO; i.compareTo(power) < 0; i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+                if(this.isInterrupted())
+                    return;
+            }
+        }
+
+        public BigInteger getResult() { return result; }
+    }
+
+
+    public static void main(String[] args) {
+        ComplexCalculation calc = new ComplexCalculation();
+        BigInteger result = calc.calculateResult(BigInteger.valueOf(2), BigInteger.valueOf(0), BigInteger.valueOf(2), BigInteger.valueOf(0));
+        System.out.println(result);
+    }
 }
--- src/DebuggingThreads.java
+++ src/DebuggingThreads.java
@@ -1,2 +1,42 @@
 public class DebuggingThreads {
+
+    public static void main (String args[]) throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Set thread name
+                Thread.currentThread().setName("WorkerThread");
+
+                //Set thread priority
+                Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+
+                System.out.println("Starting thread: " + Thread.currentThread().getName());
+                System.out.println(Thread.currentThread().getName()+" priority is: "+Thread.currentThread().getPriority());
+                throw new RuntimeException("Intentional Exception");
+            }
+        });
+
+        /*
+         * Unchecked exception will bring down whole thread if not caught and handled.
+         * That makes it difficult to debug
+         * We can use setUncaughtExceptionHandler to be able to do cleanup and log
+         * useful information for troubleshooting.
+         */
+        t1.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+               System.out.println("Critical error in thread: " + t.getName()
+                       + " the error is: " + e.getMessage());
+            }
+        });
+
+        System.out.println("Before Starting thread: " + Thread.currentThread().getName());
+        t1.start();
+        System.out.println("After Starting thread: " + Thread.currentThread().getName());
+
+        //De-schedules thread for given time - no spinning loop, no CPU utilisation
+        Thread.sleep(10000);
+
+    }
 }
